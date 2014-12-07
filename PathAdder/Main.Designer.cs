9029b681249298002f4698f3d347c1a6d333aa15
Ognjen Ivkovic <opantm2+git@gmail.com>
2014-12-07 03:39:16 -0600

    Implemented initial attempt at app.

---
+++ PathAdder/Main.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace PathAdder
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.txtPath = new System.Windows.Forms.TextBox();
+			this.cmdAppend = new System.Windows.Forms.Button();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 3;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.txtPath, 1, 0);
+			this.tableLayoutPanel1.Controls.Add(this.cmdAppend, 2, 0);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 1;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 64F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(1053, 64);
+			this.tableLayoutPanel1.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(4, 16);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(78, 31);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Path:";
+			// 
+			// txtPath
+			// 
+			this.txtPath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtPath.Location = new System.Drawing.Point(89, 13);
+			this.txtPath.Name = "txtPath";
+			this.txtPath.Size = new System.Drawing.Size(815, 38);
+			this.txtPath.TabIndex = 1;
+			this.txtPath.Click += new System.EventHandler(this.txtPath_Click);
+			this.txtPath.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtPath_KeyDown);
+			// 
+			// cmdAppend
+			// 
+			this.cmdAppend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.cmdAppend.AutoSize = true;
+			this.cmdAppend.Location = new System.Drawing.Point(910, 11);
+			this.cmdAppend.Name = "cmdAppend";
+			this.cmdAppend.Size = new System.Drawing.Size(140, 41);
+			this.cmdAppend.TabIndex = 3;
+			this.cmdAppend.Text = "Append";
+			this.cmdAppend.UseVisualStyleBackColor = true;
+			this.cmdAppend.Click += new System.EventHandler(this.cmdAppend_Click);
+			// 
+			// Main
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1053, 64);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.KeyPreview = true;
+			this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			this.Name = "Main";
+			this.Text = "Path Appender";
+			this.Load += new System.EventHandler(this.Main_Load);
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Main_KeyDown);
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button cmdAppend;
+    }
+}
+
---
+++ PathAdder/Main.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PathAdder {
+    public partial class Main : Form {
+
+        public Main() {
+            InitializeComponent();
+        }
+
+		private void Main_Load(object sender, EventArgs e) {
+			try {
+				if(Clipboard.ContainsText()) {
+					var ClipPath = Clipboard.GetText();
+					EnvironmentUtils.EnsureValidEnvironmentPath(ClipPath);
+					txtPath.Text = Clipboard.GetText();
+				}
+			} catch {
+				// Ignore any issues setting clipboard path, it's just for convenience and should not prevent program being used.
+			}
+		}
+
+		private void txtPath_KeyDown(object sender, KeyEventArgs e) {
+			if(e.KeyCode == Keys.Enter) {
+				AppendPath();
+				e.Handled = true;
+			}
+		}
+
+		private void cmdAppend_Click(object sender, EventArgs e) {
+			AppendPath();
+		}
+
+		private void AppendPath() {
+			try {
+				EnvironmentUtils.AppendPath(txtPath.Text);
+				txtPath.Text = "";
+			} catch(Exception ex) {
+				MessageBox.Show("Error appending path: " + ex.Message);
+			}
+		}
+
+		private void txtPath_Click(object sender, EventArgs e) {
+			using(var FBD = new FolderBrowserDialog()) {
+				if(!String.IsNullOrWhiteSpace(txtPath.Text)) {
+					try {
+						FBD.SelectedPath = txtPath.Text;
+					} catch {
+						// Invalid path, reset settings.
+						FBD.Reset();
+					}
+				}
+				if(FBD.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+					txtPath.Text = FBD.SelectedPath;
+			}
+		}
+
+		private void Main_KeyDown(object sender, KeyEventArgs e) {
+			if(e.KeyCode == Keys.Escape)
+				Close();
+		}
+    }
+}
