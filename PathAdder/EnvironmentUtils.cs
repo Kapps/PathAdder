d8179ae95d6d4a962b82a72b90f310284f09e3a3
Ognjen Ivkovic <opantm2+git@gmail.com>
2014-12-07 03:36:58 -0600

    Implemented project.

---
+++ PathAdder/EnvironmentUtils.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathAdder {
+    /// <summary>
+    /// Helper class for managing environment variables.
+    /// </summary>
+    public static class EnvironmentUtils {
+		/// <summary>
+		/// Appends the given path to the environment machine variable.
+		/// The path must be valid, according to the rules of <seealso cref="EnsureValidEnvironmentPath"/>.
+		/// </summary>
+		public static void AppendPath(string AppendPath) {
+			EnsureValidEnvironmentPath(AppendPath);
+			var ExistingPath = Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.Machine);
+			var NewPath = ExistingPath + ";" + AppendPath;
+			Environment.SetEnvironmentVariable("PATH", NewPath, EnvironmentVariableTarget.Machine);
+		}
+
+		/// <summary>
+		/// Checks whether the given path is a valid path that may be added to the environment path variable.
+		/// The path must be a valid directory and must exist.
+		/// If the path is invalid or does not exist, an exception is thrown.
+		/// If the path is already present in the machine environment variable, an exception is thrown.
+		/// </summary>
+		/// <param name="EnvPath"></param>
+		public static void EnsureValidEnvironmentPath(string EnvPath) {
+			var InvalidChars = Path.GetInvalidPathChars();
+			if(String.IsNullOrWhiteSpace(EnvPath))
+				throw new ArgumentException("No path specified.");
+			if(EnvPath.Any(c => InvalidChars.Contains(c)))
+				throw new ArgumentException("Path contains invalid characters in the name.");
+			if(!Directory.Exists(EnvPath))
+				throw new ArgumentException("Path does not point to a valid directory.");
+			var ExistingPath = Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.Machine);
+			if(ExistingPath.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries).Any(c => ArePathsEqual(c, EnvPath)))
+				throw new ArgumentException("Path already exists in environment variable.");
+		}
+
+		private static string NormalizePath(string path) {
+			// Courtesy of http://stackoverflow.com/questions/1266674/how-can-one-get-an-absolute-or-normalized-file-path-in-net/21058121#21058121
+			return Path.GetFullPath(new Uri(path).LocalPath)
+			   .TrimEnd(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar)
+			   .ToUpperInvariant();
+		}
+
+		private static bool ArePathsEqual(string p1, string p2) {
+			// Don't crash because of an invalid environment variable.
+			try {
+				return NormalizePath(p1) == NormalizePath(p2);
+			} catch {
+				return false;
+			}
+		}
+    }
+}
---
+++ PathAdder/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PathAdder {
+    static class Program {
+
+		private const int ATTACH_PARENT_PROCESS = -1;
+
+        [DllImport("User32.dll")]
+        private static extern bool SetProcessDPIAware();
+		[DllImport("kernel32.dll")]
+		private static extern bool AttachConsole(int dwProcessId);
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static int Main(string[] args) {
+			// For debugging, sometimes the manifest doesn't seem to apply.
+			// Errors aren't a concern and shouldn't happen anyways.
+            SetProcessDPIAware();
+
+			// Check if we're being run from the command line by passing in arguments.
+			string ArgPath = String.Join(" ", args).Trim();
+			if(!String.IsNullOrWhiteSpace(ArgPath)) {
+				try {
+					AttachConsole(ATTACH_PARENT_PROCESS); // Ignore errors, can't do anything about it.
+					EnvironmentUtils.AppendPath(ArgPath);
+					Console.WriteLine("Successfully added \'" + ArgPath + "\' to path variable.");
+					return 0;
+				} catch(Exception ex) {
+					Console.Error.WriteLine("Failed to append path: " + ex.Message);
+					return 1;
+				}
+			}
+
+			// If not, show a UI.
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Main());
+			return 0;
+        }
+    }
+}
